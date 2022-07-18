74ee76b6c98d59b3341c62d439c370af5516f361
stephenlucerne <48700284+stephenlucerne@users.noreply.github.com>
2022-07-18 11:29:32 +1000

    added IL2CPP support for windows

---
+++ Runtime/ModIO.Implementation/Classes/AotTypeEnforcer.cs
@@ -0,0 +1,21 @@
+﻿using ModIO.Implementation.API.Objects;
+using Newtonsoft.Json.Utilities;
+using UnityEngine;
+ 
+namespace ModIO.Implementation.API
+{
+	/// <summary>
+	/// You do not need to use this class. This is used to ensure specific types in API request
+	/// objects get AOT code generated when using IL2CPP compilation.
+	/// </summary>
+	internal class AotTypeEnforcer : MonoBehaviour
+	{
+		public void Awake()
+		{
+			AotHelper.EnsureList<ImageObject>();
+			AotHelper.EnsureList<MetadataKVPObject>();
+			AotHelper.EnsureList<ModTagObject>();
+			AotHelper.EnsureList<ModObject>();
+		}
+	}
+}
